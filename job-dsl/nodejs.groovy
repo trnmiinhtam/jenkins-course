d362aecd8e6aaaaa52a84a1e64c8812b52ed8ab4
trnmiinhtam <trnminhtam@yahoo.com>
2021-09-24 19:43:39 +0700

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/trnmiinhtam/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('trnminhtam@gmail.com')
         }
     }
     triggers {
